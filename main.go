f7fa11403f8b42c0e94a2a8d28726ae10b36280d
linuxandchill <rivercodi@gmail.com>
2017-08-23 08:28:43 -0700

    encoding/json

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the homepage")
@@ -14,6 +23,17 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "First", Desc: "Hello One", Content: "Article content"},
+		Article{Title: "Second", Desc: "Hello Two", Content: "Article content"},
+	}
+
+	fmt.Println("Endpoint Hit: returnAllArticles")
+
+	json.NewEncoder(w).Encode(articles)
 }
 
 func main() {
